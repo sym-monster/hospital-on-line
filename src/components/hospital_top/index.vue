14a5444df306da9428aa3c4b000e01f2b98af1e2
sym-monster <92425247+sym-monster@users.noreply.github.com>
2023-12-07 23:03:40 +0800

    添加了top

---
+++ src/components/hospital_top/index.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="top">
+        <div class="content">
+            <div class="left">
+                <img src="../../assets/images/logo.png" alt="">
+                <p>尚医通 预约挂号统一平台</p>
+            </div>
+            <div class="right">
+                <p class="help">帮助中心</p>
+                <p class="login">登录注册</p>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped lang="scss">
+    .top{
+        width: 100%;
+        height: 70px;
+        background-color: pink;
+        display:flex;
+        justify-content: center;
+        .content{
+            width: 1200px;
+            height: 70px;
+            background-color: aqua;
+        }
+    }
+</style>
